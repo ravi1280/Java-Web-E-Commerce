261384ca8ff02c0d1f405b3b60a14a9b4c476fd6
ravi1280 <ravishkaindrajith9.9@gmail.com>
2024-12-14 02:01:10 +0530

    Update user entity

--- GentEdge/src/java/entity/User.java
+++ GentEdge/src/java/entity/User.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package entity;
 
-/**
- *
- * @author ravis
- */
-public class User {
+import java.io.Serializable;
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable{
+   @Id
+   @Column(name = "id")
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  int id;
+   
+   private String firstName;
+   private String flastName;
+   private String email;
+   private String mobile;
+   private String password;
+   private String veificatiion;
+    
     
 }
