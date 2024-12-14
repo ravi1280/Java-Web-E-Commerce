29c48b6fb71b04cb45d11083458f0ad15a6205cc
ravi1280 <ravishkaindrajith9.9@gmail.com>
2024-12-14 17:37:33 +0530

    create user Entity and user_DTO file

---
+++ GentEdge/src/java/dto/User_DTO.java
@@ -0,0 +1,46 @@
+
+package dto;
+
+import java.io.Serializable;
+
+
+public class User_DTO implements Serializable{
+    
+    private String fName;
+    private String lName;
+    private String mobile;
+    private String  email;
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
--- GentEdge/src/java/entity/User.java
+++ GentEdge/src/java/entity/User.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import javax.annotation.Generated;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -13,17 +14,77 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "users")
 public class User implements Serializable{
-   @Id
-   @Column(name = "id")
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private  int id;
-   
-   private String firstName;
-   private String flastName;
-   private String email;
-   private String mobile;
-   private String password;
-   private String veificatiion;
     
+   @Id
+    @Column(name = "email", length = 100, nullable = false)
+    private String email;
+   
+   @Column(name = "fname",length = 50,nullable = false)
+   private String firstName;
+   
+   @Column(name = "lname",length = 50,nullable = false)
+   private String lastName;
+
+   @Column(name = "mobile",length = 15,nullable = false)
+   private String mobile;
+   
+   @Column(name = "password",length = 20,nullable = false)
+   private String password;
+   
+   @Column(name = "verification",length = 50,nullable = false)
+   private String veificatiion;
+
+    public User() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getVeificatiion() {
+        return veificatiion;
+    }
+
+    public void setVeificatiion(String veificatiion) {
+        this.veificatiion = veificatiion;
+    }
+    
+   
     
 }
